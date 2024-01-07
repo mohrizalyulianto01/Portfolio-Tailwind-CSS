4f326046a7702f638c441d9561bf5b44cca1c740
M Rizal Yulianto <mohrizalyulianto@gmail.com>
2024-01-07 14:25:49 +0700

    membuat hero section part1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10b981",
+        dark: "#020617",
+      },
+    },
+  },
+  plugins: [],
+};
