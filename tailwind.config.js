d926aeb029f2adb973ab819741909a2de7dc3996
M Rizal Yulianto <mohrizalyulianto@gmail.com>
2024-01-07 15:47:44 +0700

    hero section clear

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#10b981",
         dark: "#020617",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
